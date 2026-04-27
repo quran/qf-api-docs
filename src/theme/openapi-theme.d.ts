036deff2d775703b517833e00cce9a6384940c66
Basit Minhas <basit@quran.com>
2026-04-27 23:01:21 +0500

    Fix SDK onboarding docs verification

---
+++ src/theme/openapi-theme.d.ts
@@ -0,0 +1,13 @@
+declare module "@theme/ApiDemoPanel/buildPostmanRequest" {
+  const buildPostmanRequest: (...args: any[]) => any;
+  export default buildPostmanRequest;
+}
+
+declare module "@theme/ApiDemoPanel/CodeTabs" {
+  const CodeTabs: any;
+  export default CodeTabs;
+}
+
+declare module "@theme/ApiItem/hooks" {
+  export const useTypedSelector: any;
+}
